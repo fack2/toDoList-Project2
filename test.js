e24433563f9865078b4993aff0d9199fa2ccca84
Muniralsharif <munir.sniper@gmail.com>
2019-06-25 10:44:09 +0300

    adding files to master repo

---
+++ test.js
@@ -0,0 +1,12 @@
+var test = require('tape');
+var logic = require('./logic');
+
+test('Example test', function(t) {
+  t.pass();
+  t.end();
+});
+
+test('Example test', function(t) {
+    t.equal()
+    t.end();
+  });
